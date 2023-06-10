ec9556b8b310dbd4ad88cfc93afbcda5d0f7545c
wear工程师 <1225186748@qq.com>
2023-06-10 19:48:56 +0800

    replace knife4j

--- files_test.go
+++ files_test.go
@@ -6,16 +6,9 @@ import (
 
 func TestOpenFiles(t *testing.T) {
 	for _, filename := range []string{
-		"favicon-16x16.png",
-		"favicon-32x32.png",
-		"index.css",
-		"index.html",
-		"oauth2-redirect.html",
-		"swagger-initializer.js",
-		"swagger-ui.css",
-		"swagger-ui.js",
-		"swagger-ui-bundle.js",
-		"swagger-ui-standalone-preset.js",
+		"services.json",
+		"doc.html",
+		"robots.txt",
 	} {
 
 		_, err := FS.Open(filename)
